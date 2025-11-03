cc392a3d57d0ec15ecc832878f5c94e662349d30
Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-11-03 15:46:42 +0600

    feat: setup betterauth and prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
+import { authClient } from '@/lib/auth-client'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <Button />
+      <Button
+        onClick={() =>
+          authClient.signIn.email({
+            email: 'contact@shahriyar.dev',
+            password: 'password',
+            rememberMe: true,
+            callbackURL: '/idk',
+          })
+        }
+      >
+        Sign In
+      </Button>
     </div>
   )
 }
