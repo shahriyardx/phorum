fb703d05ba06de7c6378b2e444e388d0d9215141
Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-11-10 21:43:24 +0600

    fix: use suspense for useSearchParams

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import ForumLayout from '@/components/forum-layout'
+import { Suspense } from 'react'
 
 export default function RootLayout({
   children,
@@ -7,7 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <ForumLayout>
-      <main className="h-svh overflow-y-auto">{children}</main>
+      <Suspense fallback={<>...</>}>
+        <main className="h-svh overflow-y-auto">{children}</main>
+      </Suspense>
     </ForumLayout>
   )
 }
